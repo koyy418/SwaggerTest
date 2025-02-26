c812b877334a5c0e4bf548fb62e100fd326c28f9
koyy418 <younko418@gmail.com>
2025-02-26 22:57:35 +0900

    Revert "start"
    This reverts commit ec4e98d580cf4e155b7e30b6ac6b9582e558a332.

--- src/main/java/me/koyunyoung/springbootdeveloper/repository/BlogRepository.java
+++
@@ -1,7 +0,0 @@
-package me.koyunyoung.springbootdeveloper.repository;
-
-import me.koyunyoung.springbootdeveloper.domain.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogRepository extends JpaRepository<Article, Long> {
-}
